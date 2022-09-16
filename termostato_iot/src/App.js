a8590106054c7ad430f79e84b943e6c2e700f54f
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2022-09-16 12:54:06 -0300

    primeiro commit

---
+++ termostato_iot/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="conteiner">
+      <h1 className="title">Termostato IOT</h1>
+      <div className="containerDHT">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
